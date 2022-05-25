cce7b90a3c182f1b09fb8f62ca9691ddfc771835
Nishant Kumar <91384157+Nsoni2501@users.noreply.github.com>
2022-05-25 15:36:29 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,56 +8,45 @@ import java.util.function.Predicate;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
-        ArrayList<HashMap<String,String>> matchesList = convertCSVToMap("./assets/matches.csv");
+        ArrayList<HashMap<String, String>> matchesList = convertCSVToMap(
+                "/home/nishant_soni/JAVA CODES/Data Processing/src/assets/matches.csv");
+        ArrayList<HashMap<String, String>> deliveriesList = convertCSVToMap(
+                "/home/nishant_soni/JAVA CODES/Data Processing/src/assets/deliveries.csv");
 
+        matchesPlayedPerYearOfAllYear(matchesList);
 
-        ArrayList<HashMap<String,String>> deliveriesList = convertCSVToMap("./assets/deliveries.csv");
-        System.out.println("XYZ");
+        matchesWonOfAllTeamsOverAllYear(matchesList);
 
-//  1. Number of matches played per year of all the years in IPL.
-        matchesPlayed(matchesList);
-//  2. Number of matches won of all teams over all the years of IPL.
-        matchesWon(matchesList);
-//  3. For the year 2016 get the extra runs conceded per team.
-        extrasConceded(deliveriesList);
-//  4. For the year 2015 get the top economical bowlers.
-//  5. Create your own scenario (Number of matches played in each city)
-        citiesList(matchesList);
+        extraRunConcededPerTeam(deliveriesList);
+
+        // 4. For the year 2015 get the top economical bowlers.
+
+        matchesPlayedInEachCities(matchesList);
     }
 
-    private static void citiesList(ArrayList<HashMap<String, String>> matchesList) {
-        System.out.println("5. Create your own scenario (Number of matches played in each city)");
-        List<String> distinctCities = matchesList.stream()
-                .filter(distinctByKey(p -> p.get("city")))
-                .map(p -> p.get("city")).toList();
+    private static void matchesPlayedPerYearOfAllYear(ArrayList<HashMap<String, String>> matchesList) {
+        System.out.println("1. Number of matches played per year of all the years in IPL.");
+        List<String> distinctYear = matchesList.stream()
+                .filter(distinctByKey(p -> p.get("season")))
+                .map(p -> p.get("season")).toList();
 
-
-        HashMap<String,Long> ans = new HashMap<>();
-        for(String city : distinctCities) {
-            if(!city.equals("")) {
-                long count = matchesList.stream().filter(p -> p.get("city").equals(city)).count();
-                ans.put(city, count);
-            }
+        HashMap<String, Long> q1 = new HashMap<>();
+        for (String year : distinctYear) {
+            long count = matchesList.stream().filter(p -> p.get("season").equals(year)).count();
+            q1.put(year, count);
         }
-        System.out.println(ans);
+        System.out.println(q1);
     }
 
-    private static void extrasConceded(ArrayList<HashMap<String, String>> deliveriesList) {
-        System.out.println("3. For the year 2016 get the extra runs conceded per team.");
-        long extras = deliveriesList.stream().filter(p -> Integer.parseInt(p.get("match_id"))>=577 && Integer.parseInt(p.get("match_id"))<=636 && Integer.parseInt(p.get("extra_runs"))>0).map(p -> p.get("extra_runs")).count();
-        System.out.println(extras);
-    }
-
-    private static void matchesWon(ArrayList<HashMap<String, String>> matchesList) {
+    private static void matchesWonOfAllTeamsOverAllYear(ArrayList<HashMap<String, String>> matchesList) {
         System.out.println("2. Number of matches won of all teams over all the years of IPL.");
         List<String> distinctTeams = matchesList.stream()
                 .filter(distinctByKey(p -> p.get("winner")))
                 .map(p -> p.get("winner")).toList();
 
-
-        HashMap<String,Long> q1 = new HashMap<>();
-        for(String team : distinctTeams) {
-            if(!team.equals("")) {
+        HashMap<String, Long> q1 = new HashMap<>();
+        for (String team : distinctTeams) {
+            if (!team.equals("")) {
                 long count = matchesList.stream().filter(p -> p.get("winner").equals(team)).count();
                 q1.put(team, count);
             }
@@ -65,19 +54,29 @@ public class Main {
         System.out.println(q1);
     }
 
-    private static void matchesPlayed(ArrayList<HashMap<String, String>> matchesList) {
-        System.out.println("1. Number of matches played per year of all the years in IPL.");
-        List<String> distinctYear = matchesList.stream()
-                .filter(distinctByKey(p -> p.get("season")))
-                .map(p -> p.get("season")).toList();
+    private static void extraRunConcededPerTeam(ArrayList<HashMap<String, String>> deliveriesList) {
+        System.out.println("3. For the year 2016 get the extra runs conceded per team.");
+        long extras = deliveriesList
+                .stream().filter(p -> Integer.parseInt(p.get("match_id")) >= 577
+                        && Integer.parseInt(p.get("match_id")) <= 636 && Integer.parseInt(p.get("extra_runs")) > 0)
+                .map(p -> p.get("extra_runs")).count();
+        System.out.println(extras);
+    }
 
+    private static void matchesPlayedInEachCities(ArrayList<HashMap<String, String>> matchesList) {
+        System.out.println("5. Create your own scenario (Number of matches played in each city)");
+        List<String> distinctCities = matchesList.stream()
+                .filter(distinctByKey(p -> p.get("city")))
+                .map(p -> p.get("city")).toList();
 
-        HashMap<String,Long> q1 = new HashMap<>();
-        for(String year : distinctYear) {
-            long count = matchesList.stream().filter(p-> p.get("season").equals(year)).count();
-            q1.put(year,count);
+        HashMap<String, Long> ans = new HashMap<>();
+        for (String city : distinctCities) {
+            if (!city.equals("")) {
+                long count = matchesList.stream().filter(p -> p.get("city").equals(city)).count();
+                ans.put(city, count);
+            }
         }
-        System.out.println(q1);
+        System.out.println(ans);
     }
 
     public static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
@@ -91,16 +90,15 @@ public class Main {
         String headerLine = sc.nextLine();
         String[] header = headerLine.split(",");
 
-        ArrayList<HashMap<String,String>> matchesList = new ArrayList<>();
-        HashMap<String,String> matchMap;
-        while (sc.hasNextLine())
-        {
+        ArrayList<HashMap<String, String>> matchesList = new ArrayList<>();
+        HashMap<String, String> matchMap;
+        while (sc.hasNextLine()) {
             matchMap = new HashMap<>();
             String line = sc.nextLine();
-            String[] matches = line.split(",",-1);
-            for (int i=0;i<header.length;i++) {
+            String[] matches = line.split(",", -1);
+            for (int i = 0; i < header.length; i++) {
 
-                matchMap.put(header[i],matches[i]);
+                matchMap.put(header[i], matches[i]);
             }
             matchesList.add(matchMap);
         }
@@ -108,4 +106,3 @@ public class Main {
         return matchesList;
     }
 }
-
